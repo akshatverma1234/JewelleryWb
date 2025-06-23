ad11f84fd4bf68bab938f98b29362717965d04e4
Akshat Verma <vakshat613@gmail.com>
2025-06-23 12:59:49 +0530

    Make Changes in SubCategory page

--- app/subcategory/list/page.jsx
+++ app/subcategory/list/page.jsx
@@ -127,6 +127,7 @@ const SubCategoryList = () => {
                     <Chip label="Men" color="primary" />
                     <Chip label="Women" color="primary" />
                     <Chip label="Kids" color="primary" />
+                    <Chip label="Kids" color="primary" />
                   </div>
                 </TableCell>
 
