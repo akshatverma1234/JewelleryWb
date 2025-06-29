35001c9f53fde6402a271ba947bcba4c425febee
Akshat Verma <vakshat613@gmail.com>
2025-06-29 17:35:33 +0530

    added auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+
+exports.auth = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken || req?.headers?.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "No token provided. Please log in.",
+        error: true,
+        success: false,
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
+    if (!decoded) {
+      return res.status(401).json({
+        message: "Invalid token. Unauthorized access.",
+        error: true,
+        success: false,
+      });
+    }
+
+    req.userId = decoded.id;
+    next();
+  } catch (error) {
+    console.error("JWT Auth Error:", error.message);
+    return res.status(401).json({
+      message: "Unauthorized. Token invalid or expired.",
+      error: true,
+      success: false,
+    });
+  }
+};
