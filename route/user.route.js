24dfa4615ef08c6f731a4c55c5cfc164c16b969a
Akshat Verma <vakshat613@gmail.com>
2025-06-29 17:35:04 +0530

    added the login and logout route

--- route/user.route.js
+++ route/user.route.js
@@ -2,11 +2,16 @@ const express = require("express");
 const {
   registerUserController,
   verifyEmailController,
+  loginController,
+  logoutController,
 } = require("../controllers/user.controller.js");
+const { auth } = require("../middlewares/auth.js");
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUserController);
 userRouter.post("/verifyEmail", verifyEmailController);
+userRouter.post("/login", loginController);
+userRouter.get("/logout", auth, logoutController);
 
 module.exports = userRouter;
