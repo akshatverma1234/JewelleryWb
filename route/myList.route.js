2acc26256b40b1c464ac8e203f2a9709c2c18ab6
Akshat Verma <vakshat613@gmail.com>
2025-07-18 18:57:43 +0530

    Added the Get route

--- route/myList.route.js
+++ route/myList.route.js
@@ -2,9 +2,14 @@ const express = require("express");
 const { auth } = require("../middlewares/auth.js");
 const {
   addToMyListController,
+  deleteMyListController,
+  getMyListController,
 } = require("../controllers/mylist.controller.js");
 
 const myListRouter = express.Router();
 
+myListRouter.get("/", auth, getMyListController);
 myListRouter.post("/add", auth, addToMyListController);
+myListRouter.delete("/:id", auth, deleteMyListController);
+
 module.exports = myListRouter;
